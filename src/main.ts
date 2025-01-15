c44cb54be9db17f979f1634c2311b7918baf15eb
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2025-01-15 17:19:39 +0530

    add global exception filter for Prisma client and enable CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaClientExceptionFilter } from 'nestjs-prisma';
+import { HttpStatus } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+
+  // Register PrismaClientExceptionFilter to handle Prisma Client exceptions
+  // and map them to appropriate HTTP status codes
+  app.useGlobalFilters(
+    new PrismaClientExceptionFilter(httpAdapter, {
+      // Prisma Error Code: HTTP Status Response
+      P2000: HttpStatus.BAD_REQUEST,
+      P2002: HttpStatus.CONFLICT,
+      P2025: HttpStatus.NOT_FOUND,
+    }),
+  );
+
+  // Enable CORS for all routes
+  // allow all origins
+  app.enableCors();
+
+  // Set global prefix for all routes in the application "/api"
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
